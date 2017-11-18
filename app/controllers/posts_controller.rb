b7ebad0589e7e9131757d06f7dfded0370078497
dongngo0804 <dongngo0804@gmail.com>
2017-11-18 23:04:32 +0700

    Add messages index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,27 @@
 class PostsController < ApplicationController
+  layout :resolve_layout
+
   def create
     @post = Post.new(post_params)
     @post.save
   end
 
   def index
-    @post = Post.all
+    @mentors = Post.all.pluck(:recipient).uniq.compact
   end
   
   private
+
   def post_params
     params.permit(:author, :recipient, :message)
+  end
+
+  def resolve_layout
+    case action_name
+    when "index"
+      "application_index"
+    else
+      "application"
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,39 +4,68 @@ class Post < ApplicationRecord
   validates :message, presence: { message: 'Please type in your message' }
 
   MENTORS = [
-  	'UMINO SATOSHI',
-  	'Taguchi Tetsuya',
-  	'Nguyễn Ngô Mai Uyên',
-  	'Đặng Mai Hoài Lan',
+    'UMINO SATOSHI',
+    'Taguchi Tetsuya',
+    'Nguyễn Ngô Mai Uyên',
+    'Đặng Mai Hoài Lan',
 
-  	'Trương Hương Duyên',
-  	'Nguyễn Thị Như Ngọc',
-  	'Vương Vũ Kim Ngân',
+    'Trương Hương Duyên',
+    'Nguyễn Thị Như Ngọc',
+    'Vương Vũ Kim Ngân',
 
-  	'Nguyễn Xuân Vinh',
-  	'Đinh Thanh Trúc',
-  	'Đặng Bá Tới',
-  	'Võ Đức Lợi',
-  	'Phạm Thanh Hùng',
-  	
-  	
-  	'Nguyễn Võ Huy Hoàng',
-  	'Nguyễn Thanh Tuấn',
-  	'Phạm Chân Nhân',
-  	
-  	
-  	'Lê Hồng Hiếu',
-  	'Nguyễn Trọng Hữu',
-  	'Văn Thị Phương Trang',
-  	'Kiều Ngọc Hiển',
+    'Nguyễn Xuân Vinh',
+    'Đinh Thanh Trúc',
+    'Đặng Bá Tới',
+    'Võ Đức Lợi',
+    'Phạm Thanh Hùng',
+    
+    
+    'Nguyễn Võ Huy Hoàng',
+    'Nguyễn Thanh Tuấn',
+    'Phạm Chân Nhân',
+    
+    
+    'Lê Hồng Hiếu',
+    'Nguyễn Trọng Hữu',
+    'Văn Thị Phương Trang',
+    'Kiều Ngọc Hiển',
 
-  	'Vũ Văn Toàn',
-  	'Trần Hồng Thái',
-  	'Đỗ Hoàng Phúc',
-  	'Hoàng Trọng Bảo',
-  	'Nguyễn Hoàng Nam',
-  	'Lê Văn Hậu',
-  	'Lê Xuân Trung',
-  	'Ngô Hưng Đông'
+    'Vũ Văn Toàn',
+    'Trần Hồng Thái',
+    'Đỗ Hoàng Phúc',
+    'Hoàng Trọng Bảo',
+    'Nguyễn Hoàng Nam',
+    'Lê Văn Hậu',
+    'Lê Xuân Trung',
+    'Ngô Hưng Đông'
   ]
+
+  IMAGES = {
+    'UMINO SATOSHI' => 'umino',
+    'Nguyễn Ngô Mai Uyên' => 'uyen',
+    'Trương Hương Duyên' => 'duyen',
+    'Nguyễn Thị Như Ngọc' => 'ngoc',
+    'Nguyễn Xuân Vinh' => 'vinh',
+    'Đặng Bá Tới' => 'toi',
+    'Nguyễn Võ Huy Hoàng' => 'hoang',
+    'Lê Hồng Hiếu' => 'hieu',
+    'Nguyễn Trọng Hữu' => 'huu',
+    'Vũ Văn Toàn' => 'toan'
+  }
+  DISPLAY_NAMES = {
+    'UMINO SATOSHI' => 'Umino-san',
+    'Nguyễn Ngô Mai Uyên' => 'Uyên-san',
+    'Trương Hương Duyên' => 'Duyên-san',
+    'Nguyễn Thị Như Ngọc' => 'Ngọc-san',
+    'Nguyễn Xuân Vinh' => 'Vinh-san',
+    'Đặng Bá Tới' => 'Tới-san',
+    'Nguyễn Võ Huy Hoàng' => 'Hoàng-san',
+    'Lê Hồng Hiếu' => 'Hiếu-san',
+    'Nguyễn Trọng Hữu' => 'Hữu-san',
+    'Vũ Văn Toàn' => 'Toàn-san'
+  }
+
+  def self.by_mentor(mentor)
+    where(recipient: mentor).uniq
+  end
 end
