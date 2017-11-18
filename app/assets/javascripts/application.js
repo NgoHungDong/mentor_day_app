0c8a236a9a7f3c2fca73af8e2fc4ad598704f387
dongngo0804 <dongngo0804@gmail.com>
2017-11-18 21:07:56 +0700

    Add bookblock js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,77 +12,44 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require notify.min.js
+//= require modernizr.custom.79639.js
 
-$('document').ready(function(){
-  $('input[type="text"], input[type="email"], textarea, select').focus(function(){
-    var background = $(this).attr('id');
-    $('#' + background + '-form').addClass('formgroup-active');
-    $('#' + background + '-form').removeClass('formgroup-error');
-  });
-  $('input[type="text"], input[type="email"], textarea, select').blur(function(){
-    var background = $(this).attr('id');
-    $('#' + background + '-form').removeClass('formgroup-active');
-  });
+// $('document').ready(function(){
+//   $('input[type="text"], input[type="email"], textarea, select').focus(function(){
+//     var background = $(this).attr('id');
+//     $('#' + background + '-form').addClass('formgroup-active');
+//     $('#' + background + '-form').removeClass('formgroup-error');
+//   });
+//   $('input[type="text"], input[type="email"], textarea, select').blur(function(){
+//     var background = $(this).attr('id');
+//     $('#' + background + '-form').removeClass('formgroup-active');
+//   });
 
-function errorfield(field){
-  $(field).addClass('formgroup-error');
-  console.log(field); 
-}
+// function errorfield(field){
+//   $(field).addClass('formgroup-error');
+//   console.log(field); 
+// }
 
-$("#waterform").submit(function() {
-  var stopsubmit = false;
+// $("#waterform").submit(function() {
+//   var stopsubmit = false;
 
-if($('#name').val() == "") {
-  errorfield('#name-form');
-  stopsubmit=true;
-}
-if($('#email').val() == "") {
-  errorfield('#email-form');
-  stopsubmit=true;
-}
-  if(stopsubmit) return false;
-});
+// if($('#name').val() == "") {
+//   errorfield('#name-form');
+//   stopsubmit=true;
+// }
+// if($('#email').val() == "") {
+//   errorfield('#email-form');
+//   stopsubmit=true;
+// }
+//   if(stopsubmit) return false;
+// });
     
-});
+// });
 
 
 
 
 $(document).ready(function(){
-  
-  var mousePos = {};
-
- function getRandomInt(min, max) {
-   return Math.round(Math.random() * (max - min + 1)) + min;
- }
-  
-  $(window).mousemove(function(e) {
-    mousePos.x = e.pageX;
-    mousePos.y = e.pageY;
-  });
-  
-  $(window).mouseleave(function(e) {
-    mousePos.x = -1;
-    mousePos.y = -1;
-  });
-  
-  var draw = setInterval(function(){
-    if(mousePos.x > 0 && mousePos.y > 0){
-      
-      var range = 15;
-      
-      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
-      
-      var sizeInt = getRandomInt(10, 30);
-      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
-      
-      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
-      
-      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
-
-      var style = left+top+color+size;
-      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
-    }
-  }, 1);
+    Page.init();
 });
